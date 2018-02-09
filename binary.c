6134797ee52f47c54ba45eb173d14d4602bac78b
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-02-09 14:18:51 +0530

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+char a[10],i,count=0;
+printf("enter the number");
+scanf("%s",&a);
+for(i=0;n[i]!='\0';i++)
+{
+if(!(a[i]==(0||1)))
+{
+count++;
+}
+}
+if(count==0)
+{
+printf("the number is binary");
+}
+else
+{
+printf("the number is not a binary");
+}
+}
