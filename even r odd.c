80f74898f7442d64f34afd0cfc92345d9f414e08
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-02-09 15:07:29 +0530

    Create even r odd.c

---
+++ even r odd.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int x,y,n;
+	printf("\nEnter the two numbers: ");
+	scanf("%d",&x);
+	scanf("%d",&y);
+	if(x>y)
+	{
+		n=x-y;
+	}
+	else
+	{
+		n=y-x;
+	}
+	if(n%2==0)
+	{
+		printf("\nThe difference is even.");
+	}
+	else
+	{
+		printf("\nThe difference is odd. ");
+	}
+	return 0;
+}
