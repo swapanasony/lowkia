85a6674917546793109f8fab57f846470483a48e
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-02-13 15:57:35 +0530

    Create perfect.c

---
+++ perfect.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int s1,s2,i;
+printf("enter the numbers");
+scanf("%d%d",&s1,&s2);
+pro=s1*s2;
+for(i=0;i<(pro/2);i++)
+{
+if((pro(i,2)==pro)
+{
+printf("product is perfect square");
+break;
+}
+}
+}
