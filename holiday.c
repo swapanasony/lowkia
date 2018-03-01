cb807ae74bb9a0563a25e6fed60d07b149720004
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-03-01 16:06:51 +0530

    Create holiday.c

---
+++ holiday.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main(void)
+{
+	char a[300]="sunday",b[300],c[300]="saturday";
+	int i,c3=0,c1,c2=0;
+	printf("\nEnter the day..");
+	scanf("%s",&b);
+	c1=strlen(b);
+	for(i=0;b[i]!='\0';i++)
+	{
+	        if(a[i]==b[i])
+		{
+			if(i==0)
+			{
+				c3++;
+				c2++;
+			}
+			else
+			{
+				c3++;
+			}
+		}
+		else if(c[i]==b[i])
+		{
+			c2++;
+		}
+	}
+	if(c3==c1||c2==c1)
+	{
+		printf("\n%s is a Holiday",b);
+	}
+	else
+	{
+		printf("\n%s is a working day",b);
+	}
+	return 0;
+}
