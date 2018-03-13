8ced3edd067d4c004b79d0a44d0e3e265a24e222
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-03-13 15:52:43 +0530

    Create kel.c

---
+++ kel.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+float kelvin,celsius;
+printf("enter temperatue in celsius");
+scanf("%f",&celsius);
+kelvin=celsius+273.15;
+printf("kelvin = %.0f",kelvin);
+return 0;
+}
