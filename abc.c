7ff1eb4918e589c28dfbccd44afbea5a3a1dd22a
swapanasony <30830143+swapanasony@users.noreply.github.com>
2018-03-15 08:28:12 +0000

    Create abc.c

---
+++ abc.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+int x,y,z;
+printf("enter the values of x,y,z");
+scanf("%d%d%d",&x,&y,&z);
+printf("answer of (x*y)/z is %d",((x*y)/z));
+return 0;
+}
